97233d67f3f6b01730749d8a8896fb661edf2124
shportix <danilshporta@gmail.com>
2023-11-02 14:09:39 +0200

    test

--- .buildbot/android/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-cd packages/android
-buildozer android debug || exit $?
-cd ../..
-mkdir -p ../out
-mv packages/android/bin/*.apk ../out
--- .buildbot/snap/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd packages && snapcraft
-
-cd ..
-mkdir -p ../out
-mv packages/pybitmessage*.snap ../out
